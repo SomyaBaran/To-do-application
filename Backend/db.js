d4fd69c0dd47abf8032949ac34745940a07b6e3a
Somya Baranwal <116670796+SomyaBaran@users.noreply.github.com>
2025-05-31 22:31:28 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,15 +1,14 @@
-// mongodb+srv://admin:somyadb_cluster@cluster0.za01tsl.mongodb.net/todos
-
-
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://admin:somya123@cluster0.za01tsl.mongodb.net/todos")
+mongoose.connect("mongodb+srv://somya:Gr9TB3aVDDKNgu39@cluster0.za01tsl.mongodb.net/todos")
 const todoSchema = mongoose.Schema({
     title: String,
     description: String,
     completed: Boolean,
 })
 
+
 const todo = mongoose.model('todos', todoSchema); 
+
 module.exports = {
-    todo: todo
+    todo
 }
